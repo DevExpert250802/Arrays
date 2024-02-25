6f572f9bdc1281a889385738f5d1db75057435d0
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-02-25 09:56:37 +0530

    3 SUM

--- 26.cpp
+++ 26.cpp
@@ -84,7 +84,7 @@ vector<vector<int>> triplet(int n, vector<int> &nums){
 
 
 
-Time Complexity: O(N^3), where N = size of the array.
-Reason: Each of the pointers i and j, is running for approximately N times. And both the pointers k and l combined can run for approximately N times including the operation of skipping duplicates. So the total time complexity will be O(N3). 
+Time Complexity: O(NlogN)+O(N2), where N = size of the array.
+Reason: The pointer i, is running for approximately N times. And both the pointers j and k combined can run for approximately N times including the operation of skipping duplicates. So the total time complexity will be O(N2). 
 
-Space Complexity: O(1)
+Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra space to solve this problem. So, from that perspective, space complexity can be written as O(1).
