3588ceae4ba64af0281c2556e56ced8ed7aeca9a
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-03-06 20:38:14 +0530

    Remove duplicates from Sorted Array

--- 04.cpp
+++ 04.cpp
@@ -17,11 +17,12 @@ T.C.=O(N)
 S.C.=O(N)
 
 
+
 int removeDuplicates(vector<int> &arr, int n) {
 	int i=0;
 	for(int j=1;j<n;j++){
           if (arr[j] != arr[i]) {
-			  arr[i+1]=arr[j];
+	      arr[i+1]=arr[j];
               i++;
           }
         }
