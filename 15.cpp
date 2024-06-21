e831ae6a024531c34b9effaa173d2a661bc0c96e
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-06-21 10:26:17 +0530

    Longest Subarray With Sum K (Positive)

--- 15.cpp
+++ 15.cpp
@@ -57,11 +57,12 @@ Space Complexity: O(n)
   
 #include <bits/stdc++.h>
 int longestSubarrayWithSumK(vector<int> a, long long k) {
-int left=0 ;
-int right=0;
-long long sum =a[0];
+   
+int left=0 , right=0;
+long long sum = a[0];
 int maxlength=0;
 int n= a.size();
+   
 while(right<n){
    while(left<=right && sum>k){
       sum-=a[left];
@@ -70,8 +71,8 @@ while(right<n){
    if(sum==k){
       maxlength=max(maxlength,right-left+1);
    }
-   right++;
-   sum+=a[right];
+    right++;
+   if(right<n) sum+=a[right];
 }
    return maxlength;
 }
