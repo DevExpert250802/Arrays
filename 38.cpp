0cbd7b83768b2c4f81cc25dd31601abbc8e62727
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-08-02 12:11:41 +0530

    Count Inversions in an Array

--- 38.cpp
+++ 38.cpp
@@ -28,6 +28,7 @@ Space Complexity:  O(1)
 
 
 
+#include <bits/stdc++.h>
 int cnt=0;
 void merge(vector<int> &arr, int low, int mid, int high) {
     vector<int> temp; // temporary array
@@ -43,10 +44,9 @@ void merge(vector<int> &arr, int low, int mid, int high) {
         }
         else {
             temp.push_back(arr[right]);
-            cnt+=(mid-left+1); 
+            cnt+=(mid-left+1);
             right++;
-            
-           }
+        }
     }
 
     // if elements on the left half are still left //
@@ -75,14 +75,78 @@ void mergeSort(vector<int> &arr, int low, int high) {
     mergeSort(arr, mid + 1, high); // right half
     merge(arr, low, mid, high);  // merging sorted halves
 }
-
-int numberOfInversions(vector<int>&v, int n) {
-    mergeSort(v,0,n-1);
-    return cnt;
+int numberOfInversions(vector<int>&arr, int n) {
+   mergeSort(arr,0,n-1);
+   return cnt;
 }
 
 
 
 Time Complexity:   O(N*logN)
+Space Complexity:  O(1)
 
+
+
+Alternative ::
+
+
+
+
+
+#include <bits/stdc++.h>
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+     int cnt=0;
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt+=(mid-left+1);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+    return cnt;
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt=0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt+=mergeSort(arr, low, mid);  // left half
+    cnt+=mergeSort(arr, mid + 1, high); // right half
+    cnt+=merge(arr, low, mid, high);  // merging sorted halves
+    return cnt;
+}
+int numberOfInversions(vector<int>&arr, int n) {
+   return mergeSort(arr,0,n-1);
+   
+}
+
+
+Time Complexity:   O(N*logN)
 Space Complexity:  O(1)
